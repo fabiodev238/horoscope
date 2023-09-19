631d782028c9350dcba1c625360defb66458baf6
Fabio Pereira <fabiopereira20162015@gmail.com>
2023-09-19 19:15:24 -0300

    initial commit

---
+++ app/src/main/java/com/devs/horoscope/ui/home/luck/LuckFragment.kt
@@ -0,0 +1,25 @@
+package com.devs.horoscope.ui.home.luck
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.devs.horoscope.databinding.FragmentLuckBinding
+
+
+class LuckFragment : Fragment() {
+
+    private var _binding: FragmentLuckBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLuckBinding.inflate(
+            layoutInflater, container, false
+        )
+        return binding.root
+    }
+}
