0a9a19f9a3a1a3673cb6f3ebab8423ac6a3a8f7a
Fabio Pereira <fabiopereira20162015@gmail.com>
2023-09-21 18:05:54 -0300

    mvvm, navigationComponent, stateflow, ViewModel, fragment.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -36,6 +38,9 @@ android {
     buildFeatures {
         viewBinding = true
     }
+ //   kotlin {
+    //    jvmToolchain(8)
+  //  }
 }
 
 dependencies {
@@ -46,7 +51,9 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
-
+    //DaggerHilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt("com.google.dagger:hilt-compiler:2.48")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
